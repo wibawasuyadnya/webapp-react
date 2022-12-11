5e5cccc797236e9efb33a288585157e9aff9e2af
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-11 20:56:47 +0800

    fix link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Notfound from './pages/Notfound';
 
@@ -6,11 +6,13 @@ import Notfound from './pages/Notfound';
 function App() {
   return (
     <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="*" element={<Notfound/>} />
-        <Route element={<Notfound/>} />
+        <Route path="" element={<Notfound/>} />
       </Routes>
+    </Router>
     </>
   );
 }
