d2a0d0dcbdb48016c7efb817e5d998c39412eaa7
wibawasuyadnya <dekgusnfs@gmail.com>
2022-12-12 09:31:21 +0800

    update 404 responsive

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -15,12 +15,12 @@ function Notfound() {
                 <Nabvar/>
             </header>
             <main>
-                <div className="container w-full mx-auto min-h-screen flex flex-col items-center justify-center space-y-3">
+                <div className="container w-full mx-auto min-h-screen flex flex-col items-center justify-center space-y-3 xs:space-y-1 xs:p-2 xs:text-center">
                     <img src={images} width="140px" alt="logoMain" className="border-solid border-2 border-[#1a58a4] rounded-full"></img>
-                    <h1 className=" text-[color:var(--clr-secondary)] text-[150px] xs:text-[100px] xs:leading-tight pb-[10px] pt-[10px] font-inter-extra-bold">404</h1>
-                    <h2 className=" text-[color:var(--clr-secondary)] text-5xl xs:text-5xl xs:leading-tight pb-[10px] pt-[10px] font-inter-bold">Oopsy, I Think You're Lost</h2>
-                    <p className=' text-[color:var(--clr-body)] text-lg xs:text-base font-inter-medium'>Sorry, we couldn't find the pages you're looking for.</p>
-                    <p className=' text-[color:var(--clr-body)] text-lg xs:text-base font-inter-medium'>Use Sidebar to navigate back to home :3</p>
+                    <h1 className=" text-[color:var(--clr-secondary)] text-[150px] xs:text-[70px] xs:leading-tight pb-[10px] pt-[10px] font-inter-extra-bold">404</h1>
+                    <h2 className=" text-[color:var(--clr-secondary)] text-5xl xs:text-2xl xs:leading-tight pb-[10px] pt-[10px] font-inter-bold">Oopsy, I Think You're Lost</h2>
+                    <p className=' text-[color:var(--clr-body)] text-lg xs:text-sm font-inter-medium'>Sorry, we couldn't find the pages you're looking for.</p>
+                    <p className=' text-[color:var(--clr-body)] text-lg xs:text-sm font-inter-medium'>Use Sidebar to navigate back to home :3</p>
                 </div>
             </main>
             <footer>
